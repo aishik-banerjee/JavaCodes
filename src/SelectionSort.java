dbf37bdaa10fdab19966201cc6a2a07ecd120136
Aishikk Banerjee <aishikbanerjee97@gmail.com>
2025-03-03 18:14:54 +0530

    SelectionSort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,3 +1,45 @@
+import java.util.Arrays;
+
 public class SelectionSort {
     
+        public static void main(String[] args) {
+        
+        int arr[]={1,5,9,8,7,4,3,6};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void selection(int arr[]){
+
+        for(int i=0;i<arr.length;i++){
+
+            int end=arr.length - i - 1;
+            int maxIndex=getMax(arr, 0,end);
+            swap(arr,maxIndex,end);
+        }
+
+    }
+
+    public static int getMax(int arr[],int start, int end){
+
+        int max = start;
+
+        for(int i=start;i<=end;i++){
+
+            if(arr[i] >arr[max ]){
+                max=i;
+            }
+        }
+        return max;
+
+    }
+
+    public static void swap(int arr[],int max, int end){
+
+        int temp=arr[max];
+        arr[max]=arr[end];
+        arr[end]=temp;
+
+    }
 }
