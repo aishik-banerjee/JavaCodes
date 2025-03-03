b86649f61f9d6ec1ad144b9c32c8dfc58d1b63a2
Aishikk Banerjee <aishikbanerjee97@gmail.com>
2025-03-03 10:27:06 +0530

    LinearSearch code

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,3 +1,25 @@
 public class LinearSearch{
-    
+    public static void main(String[] args) {
+        int arr[]={1,5,6,9,8,7};
+        int target=7;
+        int position=Search(arr, target);
+
+        System.out.println("Index Position is " + position);
+
+        
+    }
+
+    public static int Search(int arr[], int target){
+
+        if(arr.length==0)
+        return -1;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target)
+            return i;
+        }
+
+        return -1;
+
+    }
 }
