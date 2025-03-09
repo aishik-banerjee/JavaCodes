4b5da080d61cbb52fa210ddbea5ce85cd68c139f
Aishikk Banerjee <aishikbanerjee97@gmail.com>
2025-03-09 10:24:32 +0530

    CyclicSort

--- src/CyclicSort.java
+++ src/CyclicSort.java
@@ -1,3 +1,28 @@
+import java.util.Arrays;
+
 public class CyclicSort {
     
+    public static void main(String[] args) {
+        
+        int arr[]={1,3,9,8,5,2,4,6,7};
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void cyclic(int arr[]){
+
+        int i=0;
+        int end=arr.length;
+        while (i<end) {
+            int correctIndex=arr[i]-1;
+            if( correctIndex < end && arr[i] != arr[correctIndex]){
+                int temp=arr[i];
+                arr[i]=arr[correctIndex];
+                arr[correctIndex]=temp;
+            }else{
+                i++;
+            }
+        }
+
+    }
 }
