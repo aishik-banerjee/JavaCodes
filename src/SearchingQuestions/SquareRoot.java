8815d6c675f0092bf71bfca59120442d1678c2b3
Aishikk Banerjee <aishikbanerjee97@gmail.com>
2025-03-09 12:32:37 +0530

    SquareRoot

--- src/SearchingQuestions/SquareRoot.java
+++ src/SearchingQuestions/SquareRoot.java
@@ -1,5 +1,39 @@
 package SearchingQuestions;
 
 public class SquareRoot {
+
+    public static void main(String[] args) {
+        int num=8;
+        int sqrt = mySqrt(num);
+        System.out.println("Square Root of "+ num + " is "+ sqrt);
+
+    }
+
+
+
+    public static int mySqrt(int x) {
+
+        if(x==0 || x==1)
+        return x;
+
+        int start = 1;
+        int end = x;
+        int mid = 0;
+
+        while (start <= end){
+
+            mid = start + (end - start) / 2;
+
+            if(mid > x/mid){
+                end = mid - 1;
+            }else if(mid < x/mid){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+
+        return end;
+    }    
     
 }
