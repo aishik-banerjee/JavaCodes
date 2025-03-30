e8349cc1226e0108e280d018e550ed1ddb7dd59f
Aishikk Banerjee <aishikbanerjee97@gmail.com>
2025-03-30 21:38:01 +0530

    Pattern Hourglass

--- src/Patterns/StarPatterns.java
+++ src/Patterns/StarPatterns.java
@@ -3,7 +3,7 @@ package Patterns;
 public class StarPatterns {
 
     public static void main(String[] args) {
-        Pattern10(5);
+        Pattern12(5);
     }
 
     public static void Pattern1(int n){
@@ -147,6 +147,41 @@ public class StarPatterns {
             }
             
             for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+    
+    }
+
+    public static void Pattern11(int n){
+   
+        for(int i=1;i<=n;i++){
+
+            for(int l=1;l<i;l++){
+                System.out.print(" ");
+            }
+            
+            for(int j=i;j<=n;j++){
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+    
+    }
+
+    public static void Pattern12(int n){
+   
+        for(int i=1;i<=2*n;i++){
+            int totalSpaces=i<=n?i-1:(2*n)-i;
+            int totalStars=i<=n?n-i+1:i-n;
+            for(int l=1;l<=totalSpaces;l++){
+                System.out.print(" ");
+            }
+            
+            for(int j=1;j<=totalStars;j++){
                 System.out.print("* ");
             }
             
